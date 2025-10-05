292c3809742700d09424cbf4b9ce7b53c87b71a2
SanjayJai <sanjay.dev0110@gmail.com>
2025-10-05 10:27:04 +0530

    Update 2025_10_04_104543_create_employees_table.php

--- database/migrations/2025_10_04_104543_create_employees_table.php
+++ database/migrations/2025_10_04_104543_create_employees_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade'); // Employee belongs to company
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Link to users table
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade'); 
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
             $table->string('name');
             $table->string('email')->unique(); // Employee email
      
